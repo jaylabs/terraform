bf0c2733405f1625650d0ab72d6f185d771fe44c
Jesse Cota <34288347+jaylabs@users.noreply.github.com>
2020-05-15 03:47:37 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  version = "~> 2.28"
+  profile = lookup(local.profile, local.environment)
+  region  = lookup(local.region, local.environment)
+
+}
+
+terraform {
+  required_version = "~> 0.12"
+  backend "s3" {
+    encrypt = true
+    bucket  = "terraform-remote-state"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    profile = "jaylabs-development"
+  }
+}
